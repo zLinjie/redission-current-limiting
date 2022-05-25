b698ce9ea8e2a2aa6ef3ea4bb7b89fb6076a7b6f
zvhs <d880975@163.com>
2022-05-25 20:57:16 +0800

    init

---
+++ src/main/java/com/zvhs/currentlimiting/CurrentLimitingApplication.java
@@ -0,0 +1,13 @@
+package com.zvhs.currentlimiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentLimitingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrentLimitingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zvhs/currentlimiting/CurrentLimitingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zvhs.currentlimiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrentLimitingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
